77a50b16c131f10e47e5d9ed0b14cf067daf51df
Kleber Luiz <157175058+kleberlz17@users.noreply.github.com>
2025-05-24 00:10:25 -0300

    exercicios de fixação

---
+++ src/main/java/br/com/cod3r/composite/folders/model/FileSystemItem.java
@@ -0,0 +1,6 @@
+package br.com.cod3r.composite.folders.model;
+
+public interface FileSystemItem {
+	void print(String structure);
+	
+}
--- src/main/java/br/com/cod3r/composite/swing/Client.java
+++ src/main/java/br/com/cod3r/composite/swing/Client.java
@@ -1,8 +1,33 @@
 package br.com.cod3r.composite.swing;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 public class Client {
 
 	public static void main(String[] args) {
+		
+		JLabel title = new JLabel("TITLE");
+		JLabel label = new JLabel("Label #1");
+		JButton button = new JButton("Button #1");
+		
+		JPanel panel = new JPanel(new FlowLayout());
+		panel.add(label);
+		panel.add(button);
+		
+		JFrame frame = new JFrame();
+		frame.setLayout(new BorderLayout());
+		frame.add(title, BorderLayout.NORTH);
+		frame.add(panel, BorderLayout.CENTER);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400,200);
+		frame.setVisible(true);
 		
 	}
 }
